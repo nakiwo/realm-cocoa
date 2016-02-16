4befc551dc19afe98244bd4adf96532d252bae80
kishikawa katsumi <kishikawakatsumi@mac.com>
2016-02-16 22:55:49 +0900

    Add `deleteRealmIfMigrationNeeded` property to RLMRealmConfiguration

--- Realm/ObjectStore/shared_realm.hpp
+++ Realm/ObjectStore/shared_realm.hpp
@@ -61,6 +61,8 @@ namespace realm {
 
             MigrationFunction migration_function;
 
+            bool delete_realm_if_maigration_needed = false;
+
             Config();
             Config(Config&&);
             Config(const Config& c);
